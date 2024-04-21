55e563bfe24e0ac1cc2fae5e2e2ec5c14b9d2c9a
Paula Santoyo <A01745312@tec.mx>
2024-04-21 00:00:19 -0600

    Cambios

--- src/utilities.py
+++ src/utilities.py
@@ -13,14 +13,15 @@ def load_stopwords(filepath='data/stopwords.txt'):
     with open(filepath, 'r', encoding='utf-8') as file:
         return set(file.read().split())
 
-def save_results_to_txt(all_results, filepath):
+def save_results_to_txt(all_results, plagiarism_results, filepath, ):
     """Guarda los resultados de similitud en un archivo de texto."""
     with open(filepath, 'w', encoding='utf-8') as file:
         for results in all_results:
             for result in results:
                 file.write(f"{result[0]} vs {result[1]}: {result[2]*100:.2f}% similar\n")
             # Insertar separación entre grupos de resultados
-            file.write("\n---\n\n")
+        file.write(plagiarism_results)
+        file.write("\n---\n\n")
 
 
 def save_results_to_excel(results, filepath):
